8428df645c59565049da23f894a0a9050a6ae742
tabish0211 <46350130+tabish0211@users.noreply.github.com>
2020-03-23 11:43:59 +0530

    Add files via upload

---
+++ Encapsulation.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeniferConsoleApplication
+{
+    public class Encapsulation
+    {
+        //it is not accessible outside class directly
+        private int _cutomerId;
+
+        public int CustomerId
+        {
+            get {
+            
+                return _cutomerId;
+
+            }
+            set{
+            
+                _cutomerId=value;
+            
+            }
+
+        }
+
+        //public void SetData(int id)
+        //{
+
+        //    _cutomerId = id;
+
+        //}
+
+        //public int GetData() {
+
+        //    return _cutomerId;
+        //}
+
+    }
+
+
+    public class CallEncapsulatedData {
+
+        static void Main() {
+
+            Encapsulation obj = new Encapsulation();
+            //obj.SetData(101);
+            //int res=obj.GetData();
+            //Console.WriteLine(res);
+
+            obj.CustomerId = 101;
+            Console.WriteLine(obj.CustomerId);
+            Console.ReadLine();
+
+        }
+    
+    }
+}
---
+++ InstanceAndStatic.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeniferConsoleApplication
+{
+    public class InstanceAndStatic
+    {
+        public int Id;//Instance variable
+        public static int counter;//static varaibles are not the part of instance/object
+       //Instance Methods
+        public void SetData() {
+
+            Id = 1;
+            counter = 2;
+        
+        }
+
+        public int GetData() {
+
+            return Id;
+        
+        }
+
+        public static void IamStatic() {
+            InstanceAndStatic obj = new InstanceAndStatic();
+            obj.Id = 5;
+            counter = 3;
+            
+        
+        }
+        //static void Main() {
+
+        //    InstanceAndStatic obj = new InstanceAndStatic();
+        //    Console.WriteLine(counter);
+        //    Console.WriteLine(obj.Id);          
+            
+        
+        //}
+    }
+
+    public class DemoStaicNonStaticCalls {
+
+        public void CallVariables()
+        {
+            InstanceAndStatic obj = new InstanceAndStatic();
+            Console.WriteLine(obj.Id);
+            Console.WriteLine(InstanceAndStatic.counter);
+
+        }
+
+        public void CallMethods()
+        {
+            InstanceAndStatic obj = new InstanceAndStatic();
+            obj.SetData();
+
+            InstanceAndStatic.IamStatic();
+
+        }
+    
+
+    }
+}
